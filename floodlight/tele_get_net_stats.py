9a4b7cb2880617b6a178149e73a6b7c80c540a16
Faux212 <jacobfoxe94@gmail.com>
2018-12-30 20:08:46 +1100

    adding ElasticSearch and telegraf Dockerfiles to repo

--- floodlight/tele_get_net_stats.py
+++ floodlight/tele_get_net_stats.py
@@ -6,6 +6,8 @@ sdn_con_port = '8080'
 sdn_con_url = 'http://'+sdn_con_ip+':'+sdn_con_port
 switch_url = '/wm/core/controller/switches/json'
 
+json_list = []
+
 response = requests.get(sdn_con_url + switch_url,
                          auth=('user', 'password'))
 data = response.json()
@@ -20,5 +22,9 @@ for json in data:
         data = response.json()
         if str(data) != '[None]':
             # print("Statistics returned from port number: " + str(port_id))
-            print(data)
+            # print(data)
+            json_list.append(data)
         port_id += 1
+
+as_json = json.dumps(data, default=json_dumps_default, sort_keys=False)
+print(as_json)

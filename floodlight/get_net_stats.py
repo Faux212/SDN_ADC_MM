cfac7ec4f6cf483c544e86ddecd8dd72ceeed749
Faux212 <jacobfoxe94@gmail.com>
2018-12-30 16:17:22 +1100

    testing new API script

--- floodlight/get_net_stats.py
+++ floodlight/get_net_stats.py
@@ -1,65 +1,11 @@
-import httplib
-import json
+import requests
 
-server_ip = '172.18.0.2'
+sdn_con_ip = '172.18.0.2'
+sdn_con_port = '8080'
+switch_url = '/wm/core/controller/switches/json'
 
-class rest_class(object):
+response = requests.get('http://'+sdn_con_ip+':'+sdn_con_port+switch_url,
+                         auth=('user', 'password'))
+data = response.json()
 
-    def __init__(self, server):
-        self.server = server
-
-    def get(self, data):
-        ret = self.rest_call({}, 'GET')
-        return json.loads(ret[2])
-
-    def set(self, data):
-        ret = self.rest_call(data, 'POST')
-        return ret[0] == 200
-
-    def remove(self, objtype, data):
-        ret = self.rest_call(data, 'DELETE')
-        return ret[0] == 200
-
-    def rest_call(self, data, action):
-        path = '/wm/core/controller/summary/json'
-        headers = {
-            'Content-type': 'application/json',
-            'Accept': 'application/json',
-            }
-        body = json.dumps(data)
-        conn = httplib.HTTPConnection(self.server, 8080)
-        conn.request(action, path, body, headers)
-        response = conn.getresponse()
-        ret = (response.status, response.reason, response.read())
-        print ret
-        conn.close()
-        return ret
-
-grabber = rest_class(server_ip)
-
-grabber.get(summary)
-
-
-
-# flow1 = {
-#     'switch':"00:00:00:00:00:00:00:01",
-#     "name":"flow_mod_1",
-#     "cookie":"0",
-#     "priority":"32768",
-#     "in_port":"1",
-#     "active":"true",
-#     "actions":"output=flood"
-#     }
-#
-# flow2 = {
-#     'switch':"00:00:00:00:00:00:00:01",
-#     "name":"flow_mod_2",
-#     "cookie":"0",
-#     "priority":"32768",
-#     "in_port":"2",
-#     "active":"true",
-#     "actions":"output=flood"
-#     }
-#
-# pusher.set(flow1)
-# pusher.set(flow2)
+print(data)

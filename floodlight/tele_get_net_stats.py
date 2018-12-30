0d60660f7d2c80a10ceb8a91c50495ecc5362dbe
Faux212 <jacobfoxe94@gmail.com>
2018-12-30 19:40:41 +1100

    testing new API script

---
+++ floodlight/tele_get_net_stats.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+sdn_con_ip = '172.18.0.2'
+sdn_con_port = '8080'
+sdn_con_url = 'http://'+sdn_con_ip+':'+sdn_con_port
+switch_url = '/wm/core/controller/switches/json'
+
+response = requests.get(sdn_con_url + switch_url,
+                         auth=('user', 'password'))
+data = response.json()
+
+for json in data:
+    uid = (json['switchDPID'])
+    # print("\n \n Pulling data from switch: " + uid + '\n \n')
+    port_id = 0
+    while (port_id <= 20):
+        response = requests.get(sdn_con_url + '/wm/statistics/bandwidth/'+uid+'/'+str(port_id)+'/json',
+                             auth=('user', 'password'))
+        data = response.json()
+        if str(data) != '[None]':
+            # print("Statistics returned from port number: " + str(port_id))
+            print(data)
+        port_id += 1

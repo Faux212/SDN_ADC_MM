7de5d1e2f907127533fcecca159c7f645a8b6266
Faux212 <jacobfoxe94@gmail.com>
2018-12-30 16:32:53 +1100

    testing new API script

--- floodlight/get_net_stats.py
+++ floodlight/get_net_stats.py
@@ -15,8 +15,9 @@ for json in data:
     print("Pulling data from switch: " + uid)
     port_id = 0
     while (port_id < 10):
+        print("Attempting port number: " + port_id)
         response = requests.get(sdn_con_url + '/wm/statistics/bandwidth/'+uid+'/'+port_id+'/json',
                              auth=('user', 'password'))
-         data = response.json()
-         print(data)
-         port_id += 1
+        data = response.json()
+        print(data)
+        port_id += 1

1f8a39b7d5e012bc90607a82dfc40fcc2102282f
Faux212 <jacobfoxe94@gmail.com>
2018-12-30 16:28:29 +1100

    testing new API script

--- floodlight/get_net_stats.py
+++ floodlight/get_net_stats.py
@@ -3,17 +3,16 @@ import json
 
 sdn_con_ip = '172.18.0.2'
 sdn_con_port = '8080'
+sdn_con_url = 'http://'+sdn_con_ip+':'+sdn_con_port
 switch_url = '/wm/core/controller/switches/json'
 
-uids = []
-
-response = requests.get('http://'+sdn_con_ip+':'+sdn_con_port+switch_url,
+response = requests.get(sdn_con_url + switch_url,
                          auth=('user', 'password'))
 data = response.json()
 
 for json in data:
-    # print(json)
-    uids.append(json['switchDPID'])
-
-
-print(uids)
+    uid = (json['switchDPID'])
+    response = requests.get(sdn_con_url + '/wm/core/switch/' + uid,
+                             auth=('user', 'password'))
+    data = response.json()
+    print(data)

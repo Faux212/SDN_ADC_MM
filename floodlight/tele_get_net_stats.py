7bc4adf940d0fd4a411ffb5d006d0ca029d66771
Faux212 <jacobfoxe94@gmail.com>
2019-01-02 18:15:25 +1100

    fixed kafka wrong version in dockerfile

--- floodlight/tele_get_net_stats.py
+++ floodlight/tele_get_net_stats.py
@@ -41,10 +41,10 @@ for unique_json in data:
         if str(data) != '[None]':
             # print("Statistics returned from port number: " + str(port_id))
             draft_json = (ast.literal_eval(json.dumps(data)))
-            print(draft_json[0]['bits-per-second-tx'])
+            draft_json = json.loads(draft_json, cls=Decoder)
+            # print(draft_json[0]['bits-per-second-tx'])
             json_list.append(draft_json[0])
         port_id += 1
 
 
-json_list = json.loads(json_list, cls=Decoder)
 print(json_list)

cbc5378912cd539b9c9cbe1ca505ad5d549c30b4
Faux212 <jacobfoxe94@gmail.com>
2019-01-10 12:26:41 +1100

    updated setup script

--- setup.sh
+++ setup.sh
@@ -12,7 +12,7 @@ docker pull glefevre/floodlight
 
 docker run --net SDNet_Docker --ip 172.18.0.2 \
               --name Floodlight_Controller \
-              glefevre/floodlight
+              glefevre/floodlight &
 
 echo "Pulling and Running 'Portainer_GUI' (172.18.0.4)..."
 docker pull portainer/portainer
@@ -23,7 +23,7 @@ docker run -d -p 9000:9000 \
               --net SDNet_Docker --ip 172.18.0.4 \
               --name Portainer_GUI \
               -v /var/run/docker.sock:/var/run/docker.sock \
-              -v portainer_data:/data portainer/portainer \
+              -v portainer_data:/data portainer/portainer &
 
 echo "Pulling and Running 'Mininet_Container' (172.18.0.3)..."
 docker pull iwaseyusuke/mininet
@@ -33,27 +33,106 @@ docker run -it --rm --privileged -e DISPLAY \
              --name Mininet_Container \
              -v /tmp/.X11-unix:/tmp/.X11-unix \
              -v /lib/modules:/lib/modules \
-             iwaseyusuke/mininet
+             iwaseyusuke/mininet &
 
-sudo docker run --net SDNet_Docker --ip 172.18.0.9 --restart always --name zookeeper zookeeper &
-sudo docker run --net SDNet_Docker --ip 172.18.0.6 --restart always --env ADVERTISED_PORT=9090 --env ZOOKEEPER_IP=172.18.0.9 --env ZOOKEEPER_PORT=2181 --env BROKER_ID=0 --name kafka_9090_local kafka_doc_9090  &
-sudo docker run --net SDNet_Docker --ip 172.18.0.7 --restart always --env ADVERTISED_PORT=9091 --env ZOOKEEPER_IP=172.18.0.9 --env ZOOKEEPER_PORT=2181 --env BROKER_ID=1 --name kafka_9091_local kafka_doc_9091  &
-sudo docker run --net SDNet_Docker --ip 172.18.0.8 --restart always --env ADVERTISED_PORT=9092 --env ZOOKEEPER_IP=172.18.0.9 --env ZOOKEEPER_PORT=2181 --env BROKER_ID=2 --name kafka_9092_local kafka_doc_9092  &
+echo "Pulling/Building Kafka Images..."
 
-sleep 60s
+cd dockerfiles/kafka_9090 && docker build -t kafka_doc_9090 .
+cd ../kakfa_9091 && docker build -t kafka_doc_9091 .
+cd ../kakfa_9092 && docker build -t kafka_doc_9092 .
+cd ..
+docker pull zookeeper
 
-echo " ####################################### Creating Kafka Topics #######################################"
+echo "Running Kafka Broker and Zookeeper Containers..."
+docker run --net SDNet_Docker \
+           --ip 172.18.0.9 \
+           --restart always \
+           --name zookeeper \
+           zookeeper &
+
+docker run --net SDNet_Docker \
+           --ip 172.18.0.6 \
+           --restart always \
+           --env ADVERTISED_PORT=9090 \
+           --env ZOOKEEPER_IP=172.18.0.9 \
+           --env ZOOKEEPER_PORT=2181 \
+           --env BROKER_ID=0 \
+           --name kafka_9090_local \
+           kafka_doc_9090  &
+
+docker run --net SDNet_Docker \
+           --ip 172.18.0.7 \
+           --restart always \
+           --env ADVERTISED_PORT=9091 \
+           --env ZOOKEEPER_IP=172.18.0.9 \
+           --env ZOOKEEPER_PORT=2181 \
+           --env BROKER_ID=1 \
+           --name kafka_9091_local \
+           kafka_doc_9091  &
+
+docker run --net SDNet_Docker \
+           --ip 172.18.0.8 \
+           --restart always \
+           --env ADVERTISED_PORT=9092 \
+           --env ZOOKEEPER_IP=172.18.0.9 \
+           --env ZOOKEEPER_PORT=2181 \
+           --env BROKER_ID=2 \
+           --name kafka_9092_local \
+           kafka_doc_9092  &
+
+echo "Waiting 30 seconds for Kafka to boot..."
+sleep 30s
+
+echo "Creating Kafka Topics"
 cd kafka_files
-sudo bin/kafka-topics.sh --create --zookeeper 172.18.0.9:2181 --replication-factor 3 --partitions 1 --topic SDN_Stats
+bin/kafka-topics.sh --create --zookeeper 172.18.0.9:2181 --replication-factor 3 --partitions 1 --topic SDN_Stats
 bin/kafka-configs.sh --zookeeper 172.18.0.9:2181 --entity-type topics --alter --add-config retention.ms=86400000 --entity-name SDN_Stats
 cd ..
-## sudo bin/kafka-topics.sh --describe --zookeeper 172.18.0.9:2181 --topic SDN_Stats ##
+## bin/kafka-topics.sh --describe --zookeeper 172.18.0.9:2181 --topic SDN_Stats ##
 
+echo "Pulling/Building ELK Images..."
+cd elastic && docker build -t elasticsearch_doc .
+cd ../logstash && docker build -t logstash_doc .
+cd ../kibana && docker build -t kibana_doc .
 
-sudo docker run --net SDNet_Docker --ip 172.18.0.10 --restart always --name telegraf_collector telegraf_collector &
+echo "Running ELK Containers..."
+docker run --net SDNet_Docker \
+                --ip 172.18.0.11 \
+                --restart always \
+                --log-opt max-size=50m \
+                -v elastisearch_data:/usr/share/elasticsearch/data \
+                --name elasticsearch \
+                elasticsearch_doc &
 
-sudo docker run --net SDNet_Docker --ip 172.18.0.11 --restart always --log-opt max-size=50m -v elastisearch_data:/usr/share/elasticsearch/data --name elasticsearch elasticsearch_doc &
+docker run --net SDNet_Docker \
+                --ip 172.18.0.12 \
+                --restart always \
+                --name logstash \
+                logstash_doc &
 
-sudo docker run --net SDNet_Docker --ip 172.18.0.12 --restart always --name logstash logstash_doc &
+docker run --net SDNet_Docker \
+                -p 5601:5601 \
+                --ip 172.18.0.14 \
+                --restart always \
+                --name kibana \
+                kibana_doc &
 
-sudo docker run -p 3000:3000 --net SDNet_Docker --ip 172.18.0.13 --restart always --name grafana grafana/grafana &
+echo "Building Grafana image..."
+cd ../grafana && docker build -t grafana_doc .
+
+echo "Running Grafana Container and forwarding port 3000..."
+docker run --net SDNet_Docker \
+                -p 3000:3000 \
+                --ip 172.18.0.13 \
+                --restart always \
+                --name grafana \
+                grafana_doc &
+echo "Building Telegraf Collector Image..."
+cd ../telegraf_script && docker build -t telegraf_collector
+
+echo "Running Telegraf Container..."
+docker run --net SDNet_Docker \
+           --ip 172.18.0.10 \
+           --restart always \
+           --name telegraf_collector \
+           telegraf_collector &

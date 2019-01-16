9c1e01ae7429a55f2473146fa8bcaf5938b8fa76
Faux212 <jacobfoxe94@gmail.com>
2019-01-16 18:05:31 +1100

    Auto-Config API script

--- floodlight_scripts/set_flow.py
+++ floodlight_scripts/set_flow.py
@@ -51,7 +51,7 @@ def send_request(url,payload):
 def generate_and_send_payload(switch_id,flow_name,eth_dst,cookie,priority,active,actions):
     json_template = '{"switch":"'+switch_id+'", "name":"'+flow_name+'", "eth_dst":"'+eth_dst+'", "cookie":"'+cookie+'", "priority":"'+priority+'", "active":"'+active+'", "actions":"'+actions+'"}'
     json_template = "'" + json_template + "'"
-    send_request(api_url,json)
+    send_request(api_url,json_template)
 
 get_switch_data(sdn_con_url,switch_url)
 

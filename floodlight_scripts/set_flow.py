a6ed800c2394bf06ced6b52ab726d0102625c867
Faux212 <jacobfoxe94@gmail.com>
2019-01-16 13:11:26 +1100

    Auto-Config API script

--- floodlight_scripts/set_flow.py
+++ floodlight_scripts/set_flow.py
@@ -86,6 +86,9 @@ for link in link_list:
     # switch_dict['switches'][source_sw]['links']['destination_sw'] = destination_sw
     # switch_dict['switches'][source_sw]['links']['destination_port'] = destination_port
 
+
+print(switch_dict)
+
 ## Switch information ##
 print("\n ### GETTING SWITCH INFORMATION " + str(len(switch_list)) + " SWITCHES ### \n")
 for switch in switch_list:

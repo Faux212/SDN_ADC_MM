f4227a279362faa8844148076c2e89d30df6a2ff
Faux212 <jacobfoxe94@gmail.com>
2019-01-20 14:59:45 +1100

    Fixing POST API calls

--- floodlight_scripts/set_flow.py
+++ floodlight_scripts/set_flow.py
@@ -222,7 +222,8 @@ for switch in switch_list:
                     break
                 else:
                     print(port)
-                    print(switch_dict[switch][port]['Dest_SW'])
+                    if 'Dest_SW' in switch_dict[switch][port]:
+                        print(switch_dict[switch][port]['Dest_SW'])
 
 # OKAY NOTE! NOW THAT BOUNDARY SWITCHES HAVE ALL FLOWS, GET THEM TO TELL NEIGHBOUR SWITCHES THEY CAN ACCESS THESE MACS THROUGH IT. (ALL CONNECTED SWITCHES CAN HIT THESE MACS THROUGH CONNECTED PORT)
 

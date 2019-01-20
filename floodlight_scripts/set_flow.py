3126d8630457547a2c84f2ea0c75e48487b46a1c
Faux212 <jacobfoxe94@gmail.com>
2019-01-20 15:53:32 +1100

    Fixing POST API calls

--- floodlight_scripts/set_flow.py
+++ floodlight_scripts/set_flow.py
@@ -247,7 +247,7 @@ for switch in switch_list:
 
                             new_output = print_all_connected_devices(new_neighbour_sw)
 
-
+                            print(new_neighbour_sw)
                             print("This is it   " + str(new_output))
                             for port in new_output[new_neighbour_sw]:
                                 print(new_neighbour_sw)

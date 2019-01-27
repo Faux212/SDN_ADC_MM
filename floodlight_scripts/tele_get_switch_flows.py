b81cbec3d5d8b22919dd837982ccde95de264861
Faux212 <jacobfoxe94@gmail.com>
2019-01-27 22:00:37 +1100

    Fixing set flows for all possible paths

--- floodlight_scripts/tele_get_switch_flows.py
+++ floodlight_scripts/tele_get_switch_flows.py
@@ -14,12 +14,10 @@ switch_data = response.json()
 
 for unique_json in switch_data:
     uid = (unique_json['switchDPID'])
-    port_id = 0
-    while (port_id <= 20):
-        response = requests.get(sdn_con_url + '/wm/staticflowpusher/list/'+uid+'/json',
-                             auth=('user', 'password'))
-        data = response.json()
-        print(data)
+    response = requests.get(sdn_con_url + '/wm/staticflowpusher/list/'+uid+'/json',
+                         auth=('user', 'password'))
+    data = response.json()
+    print(data)
 #         if str(data) != '[None]':
 #             draft_json = (ast.literal_eval(json.dumps(data)))
 #             json_list.append(draft_json[0])
